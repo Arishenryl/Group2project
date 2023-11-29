a3ab2529f9bdf3cec18fc9ae963f74e97606c26d
ashevtsova <152361235+ashevtsova@users.noreply.github.com>
2023-11-28 23:58:34 -0500

    Update main.R

--- main.R
+++ main.R
@@ -1,4 +1,4 @@
 library(tidyverse)
 library(ggplot2)
-
+group2dataset <- read_tsv("group2_dataset.tsv", show_col_types = FALSE)
 ggplot(mtcars, aes(x=as.factor(cyl), y=mpg)) + geom_boxplot( color= "red")
