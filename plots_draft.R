ec711b75e26f7d7494d26663ebb9c4726b65ad35
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-12-01 20:00:07 -0500

    Create plots_draft.R

---
+++ plots_draft.R
@@ -0,0 +1,27 @@
+
+ggplot(group2dataset, aes(x = Variant, y = Position, color = Instrument, size = Error_counts)) +
+  geom_segment(aes(xend = Variant, yend = 0), alpha = 0.7, size = 1) + 
+  geom_point(aes(y = 0), alpha = 0.7, size = 3) +  
+  labs(
+    title = "Spike Gene Position vs Variant",
+    x = "Variant",
+    y = "Spike Gene Position",
+    color = "Instrument",
+    linewidth = "Error Counts"  
+  ) +
+  theme_minimal() +
+  facet_wrap(~Instrument)
+
+
+
+ggplot(group2dataset, aes(x = Variant, y = Position, color = Instrument, size = Error_counts)) +
+  geom_point(alpha = 0.7) +  
+  labs(
+    title = "Spike Gene Position vs Variant",
+    x = "Variant",
+    y = "Spike Gene Position",
+    color = "Instrument",
+    size = "Error Counts"
+  ) +
+  theme_minimal() +
+  facet_wrap(~Instrument)
