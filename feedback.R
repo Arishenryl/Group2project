a232e89d62193f8da4916b81e99ab28c1413b83e
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-12-05 23:43:35 -0500

    Update feedback.R

--- feedback.R
+++ feedback.R
@@ -8,6 +8,9 @@
 library(tidyverse)
 library(ggplot2)
 library(stats)
+library(ggpubr)
+library(rstatix)
+install.packages("ggpubr")
 
 # Read the dataset from a tsv file
 group2dataset <- read_tsv("group2_dataset.tsv", show_col_types = FALSE)
@@ -17,6 +20,27 @@ glimpse(group2dataset)
 view(group2dataset)
 
 #plots
+#1st hypothesis
+
+#2nd hypothesis 
+
+
+#statisitcal analysis 
+
+#T-test
+t.test(data=group2dataset, Error_counts ~ Instrument)
+
+#ANOVA
+anovag2_result <- aov(Error_counts ~ Position * Variant, data = group2dataset)
+summary(anovag2_result)
+
+
+
+
+
+
+
+
 # Possible 3rd hypothesis- Alina & Sidorela
 ggplot(group2dataset, aes(x = Variant, y = Error_counts)) +
     
@@ -25,11 +49,3 @@ ggplot(group2dataset, aes(x = Variant, y = Error_counts)) +
     
     # Add jittered points for each data point to show individual observations
     geom_jitter(shape = 20)
-
-#statisitcal analysis 
-
-#T-test
-
-#ANOVA
-anovag2_result <- aov(Error_counts ~ Position * Variant, data = group2dataset)
-summary(anovag2_result)
