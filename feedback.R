458754f67a81db41fceb25f60d3f15d38cf49c63
Arishenryl <95382595+Arishenryl@users.noreply.github.com>
2023-12-05 21:58:03 -0500

    Update feedback.R

--- feedback.R
+++ feedback.R
@@ -1,1 +1,16 @@
+#Group2project.R
+#Authors @Arishenryl, @ashevtsova, @ATRP116, @ellasterkaj, @saherah
+#Purpose - pending
+#Date Dec 6th 2023
+#Version #1
 
+# Load required libraries
+library(tidyverse)
+library(ggplot2)
+
+# Read the dataset from a tsv file
+group2dataset <- read_tsv("group2_dataset.tsv", show_col_types = FALSE)
+
+# Display a summary & View the dataset interactively
+glimpse(group2dataset) 
+view(group2dataset)
